388e6f90b3e117bf3dccbd9b8c137a04930143e9
wentaoyuan <wentaoyuan@trip.com>
2026-03-14 20:43:51 +0800

    chore: remove redundant type annotations from constants
    TypeScript infers string types from pkg.name and pkg.version.

--- src/telemetry/constants.ts
+++ src/telemetry/constants.ts
@@ -7,8 +7,8 @@
 
 import pkg from '../../package.json'
 
-export const TRACER_NAME: string = pkg.name
-export const TRACER_VERSION: string = pkg.version
-export const LOGGER_NAME: string = pkg.name
+export const TRACER_NAME = pkg.name
+export const TRACER_VERSION = pkg.version
+export const LOGGER_NAME = pkg.name
 export const DEFAULT_MAX_LEN = 256
 export const SHUTDOWN_TIMEOUT_MS = 5_000
